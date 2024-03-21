485ded1ec3b7d0090375a2f1a5f2e10953882ac0
MykhayloVoroshylov <161176411+MykhayloVoroshylov@users.noreply.github.com>
2024-03-21 18:43:48 -0400

    Add files via upload

---
+++ python 2 file; rock, paper, scissors.py
@@ -0,0 +1,30 @@
+play=True
+while play==True:
+    from random import randint
+
+    player=input("rock(r), paper(p), or scissors(s)? ")
+    print(player, "vs")
+
+    chosen=randint(1,3)
+
+    if chosen==1:
+        computer='r'
+    elif chosen==2:
+        computer='p'
+    else:
+        computer='s'
+    print(computer)
+
+    if player==computer:
+        print("Draw")
+    elif player=='r' and computer=='s' or player=='s' and computer=='p' or player=='p' and computer=='r':
+        print("Player Wins")
+    else:
+        print("Computer Wins")
+    playagain=input("Do you want to play again yes/no? :")
+    if playagain=="yes":
+        play=True
+    elif playagain=="no":
+        play=False
+    else:
+        play=False
